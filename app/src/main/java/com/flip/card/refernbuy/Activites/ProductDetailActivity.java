48a51c386748ea8a9427495fcddf3f2c4fb1ed14
sachinmalik <sachin.15085@ggnindia.dronacharya.info>
2019-05-26 21:04:51 +0530

    Product detail screen

---
+++ app/src/main/java/com/flip/card/refernbuy/Activites/ProductDetailActivity.java
@@ -0,0 +1,15 @@
+package com.flip.card.refernbuy.Activites;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.flip.card.refernbuy.R;
+
+public class ProductDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail);
+    }
+}
